bd2241e8e575dbd0cc1757095dd1f693768ba695
alex <alex-just2200@yandex.ru>
2019-09-22 22:30:15 +0300

    добавление формы

--- short/blueprint.py
+++ short/blueprint.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from .forms import Shortner
 
 short_url = Blueprint('short_url', __name__, template_folder='templates')
 
-@short_url.route('/')
+@short_url.route('/', methods=['GET', 'POST'])
 def short():
     form = Shortner()
-
+    if form.validate_on_submit():
+        value = form.shortner_name.data
+        flash('Новая ссылка готова - sh.su/{}'.format(value))
+        return render_template('short_url/short.html', form = form)
     return render_template('short_url/short.html', form = form)
--- short/forms.py
+++ short/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class Shortner(FlaskForm):
     URL = StringField('Ссылка', validators=[DataRequired()])
-    shortner_name = PasswordField('Имя для вашей ссылки', validators=[DataRequired()])
+    shortner_name = StringField('Имя для вашей ссылки', validators=[DataRequired()])
     submit = SubmitField('Генерировать')
