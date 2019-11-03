f79182c3b1363e1b9f0ca20a5bfdab5ba9cf986c
alex <alex-just2200@yandex.ru>
2019-11-03 20:10:16 +0300

    добавлена настройка миграций

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 
 from short.blueprint import short_url
-
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 
 app = Flask(__name__)
 
@@ -12,5 +13,9 @@ app.config.from_object(DevelopConfig)
 # инициализируем объект БД
 pymysql.install_as_MySQLdb()
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 app.register_blueprint(short_url, url_prefix='/sh')
